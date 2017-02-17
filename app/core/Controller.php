c149687c231458fc09cfd4dc03da68ac0abaeb22
Nikola Misic <mishke.deejay@gmail.com>
2017-02-17 23:25:10 +0100

    Files added

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -1,7 +1,18 @@
 <?php
 
 class Controller {
-    
+    public function view($view, $data = [])
+	{
+		require_once '../app/views/' . $view . '.php';
+	}
+
+	public function model($model)
+	{
+		require_once '../app/models/' . $model . '.php';
+
+		return new $model();
+	}
 }
 /* 
 Pravimo metod u base Controller-u klasi  koji ce nam omoguciti da ucitamo modul i u Controller-u cemo moci nesto da uradimo s njim,promenimo,updatujemo...
+*/ 
---
+++ app/views/contact/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h1>Ovo je contact/index</h1>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem modi, sunt quo dolor sed officiis repudiandae, sint earum dolorem eligendi cumque enim voluptatem omnis doloribus, alias corporis est qui deserunt.</p>
+	<p><em><?php echo $data['email']; ?></em></p>
+
+</body>
+</html>
