40d0feb80906847d94792a357b7521bffe025a81
Nikola Misic <mishke.deejay@gmail.com>
2017-02-18 22:46:45 +0100

    Update Login.php

--- app/models/Login.php
+++ app/models/Login.php
@@ -23,17 +23,9 @@ class Login {
     public function getlogin() {
         if(($this->username != null) && ($this->password != null)) {
 
-<<<<<<< HEAD
             if (($this->username == 'admin') && ($this->password == 'admin')) {
-=======
-<<<<<<< HEAD
-            if(($_REQUEST['username']=='admin') && ($_REQUEST['password']=='admin')) {
-=======
-            if (($_REQUEST['username'] == 'admin') && ($_REQUEST['password'] == 'admin')) {
->>>>>>> 49c00874b329fd85a4c342a4a31058b9bf935e29
->>>>>>> 8dd45278608ec159ed56e248d8b776762b83a3bf
                 return 'login';
-            } else{
+            } else {
                 return 'Invalid User';
             }
         }
