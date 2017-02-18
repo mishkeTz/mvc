2e971fb6e1dea938400247c774b1461efebfb890
Nikola Misic <mishke.deejay@gmail.com>
2017-02-18 22:48:50 +0100

    Update signup.php

--- app/controllers/signup.php
+++ app/controllers/signup.php
@@ -9,13 +9,4 @@ class Signup extends Controller {
         $this->view("signup/index");
     }
 
-<<<<<<< HEAD
 }
-
-=======
-    public function phone() 
-    {
-        echo "Phone contacts";
-    }
-}
->>>>>>> 8dd45278608ec159ed56e248d8b776762b83a3bf
